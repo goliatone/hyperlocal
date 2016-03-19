fccc57ac1bab2e46e1cf338b12a3e17f79e8dfc1
goliatone <burgosemi@gmail.com>
2016-03-19 14:11:36 -0400

    server: WIP socketes

--- hyperlocal-server/io.js
+++ hyperlocal-server/io.js
@@ -1,10 +1,23 @@
 'use strict';
-
+var io;
 module.exports = function(app, config){
     var server = app.server;
     io = require('socket.io')(server);
 
     io.on('connection', function(socket){
+        console.log('connection');
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+        socket.on('chat message', function(msg){
+            console.log('message: ' + msg);
+        });
+        socket.on('beacon.add', function(data){
+            console.log('We have a visitor', data);
+        });
 
+        socket.on('beacon.remove', function(data){
+            console.log('We\'ve lost a visitor', data);
+        });
     });
 };
