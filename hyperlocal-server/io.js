95fc38cfa92440aeab2599da68843fd2b0636ee9
goliatone <burgosemi@gmail.com>
2016-03-19 18:09:46 -0400

    io: adding basic web socket endpoints

--- hyperlocal-server/io.js
+++ hyperlocal-server/io.js
@@ -6,17 +6,25 @@ module.exports = function(app, config){
 
     io.on('connection', function(socket){
         console.log('connection');
+
+        socket.on('device.register', function(data){
+            console.log('device registered', data);
+        });
+
         socket.on('disconnect', function(){
             console.log('user disconnected');
         });
+
         socket.on('chat message', function(msg){
             console.log('message: ' + msg);
         });
-        socket.on('beacon.add', function(data){
+        
+        socket.on('device.add', function(data){
             console.log('We have a visitor', data);
+
         });
 
-        socket.on('beacon.remove', function(data){
+        socket.on('device.remove', function(data){
             console.log('We\'ve lost a visitor', data);
         });
     });
