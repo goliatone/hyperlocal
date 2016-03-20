77d6ded1e77e0fad66052172b69f7445f0f59c34
goliatone <burgosemi@gmail.com>
2016-03-20 00:08:05 -0400

    visor: :shit:...

--- hyperlocal-visor/www/js/index.js
+++ hyperlocal-visor/www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var socket;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -28,11 +29,38 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+    register: function(){
+        console.log('Register device with uuid', '000000000000000000000000000000003339');
+        console.log('sockete', io);
+        var url = 'http://hyperlocal.ngrok.io';
+        socket = io(url, {
+    		transports: [
+    			'polling',
+    			'websocket',
+    			'htmlfile',
+    			'xhr-polling',
+    			'jsonp-polling'
+    		]
+    	});
+        socket.on('connect', function() {
+            console.log('We are connected');
+            socket.emit('device.register', {
+                uuid: '000000000000000000000000000000003339'
+            });
+        });
+        socket.on('error', function(e){
+            console.error('Sockete error', e);
+        });
+        socket.connect();
+        app.webview = cordova.EbWebview.open(0,encodeURI('http://hyperlocal.ngrok.io/service'), 'left=0,top=0,width=420,height=800');
+    },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        console.log('On Device Ready')
+        app.register();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -49,3 +77,55 @@ var app = {
 };
 
 app.initialize();
+
+//com.unarin.cordova.beacon
+
+var uuid = '00000000-0000-0000-0000-000000000000';
+var identifier = 'advertisedBeacon';
+var minor = 333;
+var major = 9;
+var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+/*
+ * Request iBeacon permissions
+ * For iOS 8 or higher
+ */
+cordova.plugins.locationManager.requestWhenInUseAuthorization();
+// cordova.plugins.locationManager.requestAlwaysAuthorization();
+
+// The Delegate is optional
+var delegate = new cordova.plugins.locationManager.Delegate();
+
+// Event when advertising starts (there may be a short delay after the request)
+// The property 'region' provides details of the broadcasting Beacon
+delegate.peripheralManagerDidStartAdvertising = function(pluginResult) {
+    console.log('peripheralManagerDidStartAdvertising: '+ JSON.stringify(pluginResult.region));
+};
+// Event when bluetooth transmission state changes
+// If 'state' is not set to BluetoothManagerStatePoweredOn when advertising cannot start
+delegate.peripheralManagerDidUpdateState = function(pluginResult) {
+    console.log('peripheralManagerDidUpdateState: '+ pluginResult.state);
+};
+
+cordova.plugins.locationManager.setDelegate(delegate);
+
+// Verify the platform supports transmitting as a beacon
+cordova.plugins.locationManager.isAdvertisingAvailable()
+    .then(function(isSupported){
+        if (isSupported) {
+            cordova.plugins.locationManager.startAdvertising(beaconRegion)
+                .fail(function(e){
+                    console.log('KAKA:startAdvertising fucked up');
+                    console.error(arguments);
+                })
+                .done();
+            console.log('we are here, yo!');
+        } else {
+            console.log('Advertising not supported');
+        }
+    })
+    .fail(function(e){
+        console.log('KAKA:isAdvertisingAvailable fucked up');
+        console.error(arguments);
+    });
+    .done();
