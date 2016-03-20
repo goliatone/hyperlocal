b88c895c27f7ffce8323e5f2f33c22c88f455b3c
goliatone <burgosemi@gmail.com>
2016-03-20 11:00:53 -0400

    server: route service use hyperlocal view

--- hyperlocal-server/routes/service.js
+++ hyperlocal-server/routes/service.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Service Discovered' });
+  res.render('hyperlocal', { title: 'Hyperlocal: Service Discovered' });
 });
 
 module.exports = function(app, config){
