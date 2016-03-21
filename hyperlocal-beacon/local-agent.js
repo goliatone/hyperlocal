68a9e471d54c146d6b66e91a2ac8383c14fe0283
goliatone <burgosemi@gmail.com>
2016-03-21 16:48:30 -0400

    beacon: update agent

--- hyperlocal-beacon/local-agent.js
+++ hyperlocal-beacon/local-agent.js
@@ -1,8 +1,15 @@
 var bleacon = require('bleacon');
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-var unirest = require('unirest');
-var location = process.env.RPI_LOCATION || 1;
+var eventEmitter = new (require('events').EventEmitter)();
+
+
+var SPACE_ID = process.env.HYPE_SPACE_ID || 'demo-1';
+//SERVICES should be an array of urls of services
+//running on this device.
+var SERVICES = process.env.HYPE_SERVICES;
+var SPACE_UUID = process.env.SPACE_UUID || 'demo-space-uuid';
+var DEVICE_UUID = process.env.DEVICE_UUID || 'demo-device-uuid';
+
+var socket;
 
 console.log('Started... Discovering iBeacons!');
 
@@ -14,66 +21,104 @@ bleacon.startScanning();
 var insideBeacons = [];
 var beacons = {};
 
-// Discover event
+/**
+ * Main process to monitor all beacons seen by
+ * our device.
+ */
 bleacon.on('discover', function(beacon) {
     // Our UUID is composed by the uuid + major + minor
     var UUID = beacon.uuid + beacon.major.toString() + beacon.minor.toString();
 
-    if( !beacons.hasOwnProperty(UUID)){
+    if(!beacons.hasOwnProperty(UUID)){
         beacons[UUID] = new Date();
+    }
+
+    if(UUID === 'b9407f30f5f8466eaff925556b57fe6d11'){
         console.log('UUID', UUID, beacon.proximity);
     }
 
+    //We have actually checked in with the beacon, like a swipe.
     if (beacon.proximity == 'immediate') {
-
+        // Emit event of a near beacon
+        eventEmitter.emit('beaconCheckIn', {
+            UUID: UUID,
+            range: 'immediate'
+        });
+    } else if (beacon.proximity == 'near') {
         // Emit event of a near beacon
         eventEmitter.emit('beaconIsNear', {
-            UUID: UUID
+            UUID: UUID,
+            range: 'near'
         });
-
-
     } else if (beacon.proximity == 'far') {
-
+//        console.log('beaconIsFar', UUID, beacon.proximity);
         // Emit event of a far beacon
         eventEmitter.emit('beaconIsFar', {
-            UUID: UUID
+            UUID: UUID,
+            range: 'far'
         });
-
     }
 });
 
+eventEmitter.on('beaconCheckIn', function(beacon){
+    console.log('Check in');
+    socket.emit('device.check-in', getPayload(beacon));
+});
+
 eventEmitter.on('beaconIsNear', function(beacon) {
+    if(insideBeacons.length && isAlreadyInside(beacon)){
+        return console.log('Beacon already registered');
+    }
     // Start situation, nobody is connected
-    if (insideBeacons.length === 0) {
-        // Add the beacon to the array of people that is inside
-        insideBeacons.push(beacon.UUID);
-        console.log('Currently we have ', insideBeacons);
-    }
-    // Somebody was already inside since insideBeacons != 0
-    else {
-        if (isAlreadyInside(beacon) === false) {
-            console.log('Currently we have ', insideBeacons);
-            // Add the beacon to the array of people that is inside
-            insideBeacons.push(beacon.UUID);
-        }
-    }
+    // Add the beacon to the array of people that is inside
+    insideBeacons.push(beacon.UUID);
+    socket.emit('device.near', getPayload(beacon));
+    console.log('Currently we have ', insideBeacons);
 });
 
 
 eventEmitter.on('beaconIsFar', function(beacon) {
     insideBeacons.forEach(function(insideBeacon) {
-        if (insideBeacon == beacon.UUID) {
+        if (insideBeacons !== beacon.UUID) return;
             var index = insideBeacons.indexOf(insideBeacon);
             if (index > -1) {
+                beacons[beacon.UUID] = undefined;
                 insideBeacons.splice(index, 1);
+                console.log('Removing UUID', beacon.UUID);
+                socket.emit('device.remove', getPayload(beacon));
             }
             console.log('Currently we have ', insideBeacons);
-        }
     });
 });
 
+function getPayload(beacon){
+    return {
+        id: SPACE_ID,
+        uuid: beacon.UUID,
+        services: SERVICES,
+        space: SPACE_UUID,
+        device: DEVICE_UUID
+    };
+}
+
 /////////////////////////
-///
+/// Sockete stuff
+/////////////////////////
+socket = require('socket.io-client')('http://hyper-local.ngrok.io');
+socket.on('connect', function(){
+    console.log('connect');
+});
+
+socket.on('event', function(data){
+    console.log('event', data);
+});
+
+socket.on('disconnect', function(){
+    console.log('disconnect');
+});
+
+/////////////////////////
+/// Helper functions
 /////////////////////////
 // Check if a beacon is already inside
 function isAlreadyInside(beacon) {
